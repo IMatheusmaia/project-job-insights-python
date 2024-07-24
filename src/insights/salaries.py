c8004553476dab177a0649556ab0a154c1047a96
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-07-24 20:00:08 -0300

    implementando métodos max e min da classe ProcessSalaries

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,10 +7,20 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        salaries = [
+            int(job["max_salary"])
+            for job in self.jobs_list
+            if job["max_salary"] and job["max_salary"].isdigit()
+        ]
+        return max(salaries, default=0)
 
     def get_min_salary(self) -> int:
-        pass
+        salaries = [
+            int(job["min_salary"])
+            for job in self.jobs_list
+            if job["min_salary"] and job["min_salary"].isdigit()
+        ]
+        return min(salaries, default=0)
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
