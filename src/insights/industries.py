9b2df7b49785e8c12e69f2d187ad201c4489e052
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-07-24 19:07:05 -0300

    implementando ProcessIndustries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -5,6 +5,9 @@ from typing import List
 class ProcessIndustries(ProcessJobs):
     def __init__(self):
         super().__init__()
+        self.jobs_list = []
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        return list(
+            {row["industry"] for row in self.jobs_list
+             if row["industry"]})
